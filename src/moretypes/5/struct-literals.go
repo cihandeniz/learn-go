554fe2450bdc218775fcaa56693cb8a35501a42a
Cihan Deniz <cih.deniz@gmail.com>
2021-09-14 16:09:42 +0300

    update to moretypes/5

--- src/moretypes/5/struct-literals.go
+++ src/moretypes/5/struct-literals.go
@@ -7,12 +7,13 @@ type Vertex struct {
 }
 
 var (
-	v1 = Vertex{1, 2}  // has type Vertex
-	v2 = Vertex{X: 1}  // Y:0 is implicit
-	v3 = Vertex{}      // X:0 and Y:0
-	p  = &Vertex{1, 2} // has type *Vertex
+	v1 = Vertex{1, 2}       // has type Vertex
+	v2 = Vertex{X: 1}       // Y:0 is implicit
+	v3 = Vertex{}           // X:0 and Y:0
+	v4 = Vertex{Y: 1, X: 2} // reverse order
+	p  = &Vertex{1, 2}      // has type *Vertex
 )
 
 func main() {
-	fmt.Println(v1, v2, v3, p)
+	fmt.Println(v1, v2, v3, v4, p)
 }
