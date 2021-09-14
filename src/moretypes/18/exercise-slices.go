2cbf6b29eb8e1e3ec1140af5d9fb901d5e131c17
Cihan Deniz <cih.deniz@gmail.com>
2021-09-14 19:41:00 +0300

    moretypes/18

---
+++ src/moretypes/18/exercise-slices.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"math"
+
+	"golang.org/x/tour/pic"
+)
+
+func Pic(dx, dy int) (result [][]uint8) {
+	result = make([][]uint8, dy)
+	for y := range result {
+		result[y] = make([]uint8, dx)
+
+		for x := range result[y] {
+			result[y][x] = uint8(math.Abs(float64(dx/2-x)) + math.Abs(float64(dy/2-y)))
+		}
+	}
+	return
+}
+
+func main() {
+	pic.Show(Pic)
+}
